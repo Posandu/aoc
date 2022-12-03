ceed354f5634610bba3b63f0736bf991db3eb49a
Posandu Mapa <posandumapa@gmail.com>
2022-12-03 10:54:27 +0530

    Add d3

---
+++ d3/p1.js
@@ -0,0 +1,34 @@
+const inp = require("./inp.js");
+
+const priorities = {};
+
+const simpleAlphabet = "abcdefghijklmnopqrstuvwxyz".split``;
+const capitalAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split``;
+
+const alphabet = [...simpleAlphabet, ...capitalAlphabet];
+
+alphabet.map((l, i) => (priorities[l] = i + 1));
+
+const log = console.log;
+
+const inSplit = inp.split`\n`
+
+let col = []
+
+inSplit.map((line) => {
+    const len = line.length;
+    const left = line.slice(0, len / 2);
+    const right = line.slice(len / 2);
+
+    const common = [...left].filter((l) => right.includes(l));
+
+    col.push([...new Set(common)].flat());
+})
+
+
+let tot = 0;
+
+col.map((l) => (tot += priorities[l]));
+log(tot)
+
+
---
+++ d3/p2.js
@@ -0,0 +1,32 @@
+const inp = require("./inp.js");
+
+const priorities = {};
+
+const simpleAlphabet = "abcdefghijklmnopqrstuvwxyz".split``;
+const capitalAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split``;
+
+const alphabet = [...simpleAlphabet, ...capitalAlphabet];
+
+alphabet.map((l, i) => (priorities[l] = i + 1));
+
+const log = console.log;
+
+const chunk = (a, n) =>
+	[...Array(Math.ceil(a.length / n))].map((_, i) => a.slice(n * i, n + n * i));
+
+// Split into 3 chunks
+const inSplit = chunk(inp.split`\n`, 3);
+
+let col = [];
+
+inSplit.map((line) => {
+    const common = [...line[0]].filter((l) => line[1].includes(l) && line[2].includes(l)); // Bad code, but it works
+
+    col.push([...new Set(common)].flat());
+})
+
+let tot = 0;
+
+col.map((l) => (tot += priorities[l]));
+
+log(tot)
